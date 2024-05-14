d2db276797fcb5525099d6ba75e736df748525c3
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-14 14:20:37 +0600

    basic calculator make practice

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,45 @@
+import java.util.Scanner
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+interface Calculator {
+    fun add(a: Int, b: Int): Int
+    fun subtract(a: Int, b: Int): Int
+    fun multiply(a: Int, b: Int): Int
+    fun divide(a: Int, b: Int): Int
+}
+
+class BasicCalculator : Calculator{
+    override fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    override fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    override fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    override fun divide(a: Int, b: Int): Int {
+        if (b != 0) {
+            return a / b
+        } else {
+            throw ArithmeticException("Cannot divide by 0")
+        }
+    }
+
+
+}
+
+
 fun main() {
 
+    val scanner = Scanner(System.`in`)
+    val operatorOne = scanner.nextInt()
+    val operatorTwo = scanner.nextInt()
 
-println("Hello world")
 
 
 }
