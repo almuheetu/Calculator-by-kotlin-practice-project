2b0d1f4f2b9106474c810975acc72e93518f20ac
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-14 16:14:29 +0600

    practice calculator practice

--- src/Main.kt
+++ src/Main.kt
@@ -3,43 +3,69 @@ import java.util.Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 interface Calculator {
-    fun add(a: Int, b: Int): Int
-    fun subtract(a: Int, b: Int): Int
-    fun multiply(a: Int, b: Int): Int
-    fun divide(a: Int, b: Int): Int
+    fun add(a: Int, b: Int)
+    fun subtract(a: Int, b: Int)
+    fun multiply(a: Int, b: Int)
+    fun divide(a: Int, b: Int)
 }
 
-class BasicCalculator : Calculator{
-    override fun add(a: Int, b: Int): Int {
-        return a + b
+class BasicCalculator() : Calculator {
+    override fun add(a: Int, b: Int) {
+        val result = a + b
+
+        println("add is $result")
     }
 
-    override fun subtract(a: Int, b: Int): Int {
-        return a - b
+    override fun subtract(a: Int, b: Int) {
+        val result = a - b
+
+        println("subtract is $result")
     }
 
-    override fun multiply(a: Int, b: Int): Int {
-        return a * b
+    override fun multiply(a: Int, b: Int) {
+        val result = a * b
+
+        println("multiply is $result")
     }
 
-    override fun divide(a: Int, b: Int): Int {
+    override fun divide(a: Int, b: Int) {
         if (b != 0) {
-            return a / b
+            val divide = a / b
+
+            println("devide is $divide")
         } else {
-            throw ArithmeticException("Cannot divide by 0")
+            println("Cannot divide by 0")
+
         }
     }
 
-
+    private fun result(message: String) {
+        println(message)
+    }
 }
 
 
 fun main() {
 
     val scanner = Scanner(System.`in`)
+
+    println("Enter the first operand:")
     val operatorOne = scanner.nextInt()
+
+    println("Enter the second operand:")
     val operatorTwo = scanner.nextInt()
 
+    val basicCalculator = BasicCalculator()
+
+    basicCalculator.subtract(operatorOne, operatorTwo)
+
+    basicCalculator.add(operatorOne, operatorTwo)
+
+
+    basicCalculator.multiply(operatorOne, operatorTwo)
+
+
+    basicCalculator.divide(operatorOne, operatorTwo)
 
 
 }
